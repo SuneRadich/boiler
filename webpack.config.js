50c98a37cac02dfe48114f13f91f7be5a41fe8f9
Sune Radich Vestergaard <srv@1508.dk>
2017-08-02 15:28:40 +0200

    Format file based on new .editorconfig settings
    To use spaces instead of tabs

--- webpack.config.js
+++ webpack.config.js
@@ -15,18 +15,18 @@ const PROD = ENV === 'build';
 
 //Setup default config object
 const config = {
-	plugins: []
+    plugins: []
 };
 
 /**
-* Resolve
-* Reference: https://webpack.js.org/configuration/resolve/
-*/
+ * Resolve
+ * Reference: https://webpack.js.org/configuration/resolve/
+ */
 config.resolve = {
-	alias: {
-		//Makes relative paths in the foundation setup easier to manage
-		'foundation.': 'node_modules/foundation-sites/js/entries/'
-	}
+    alias: {
+        //Makes relative paths in the foundation setup easier to manage
+        'foundation.': 'node_modules/foundation-sites/js/entries/'
+    }
 };
 
 /**
@@ -34,24 +34,21 @@ config.resolve = {
  * Reference: https://webpack.js.org/configuration/entry-context/#entry
  */
 config.entry = TEST ?
-	void 0 :
-	{
-		app: [
-			'./app/app.js',
-		]
-	};
+    void 0 : {
+        app: [
+            './app/app.js',
+        ]
+    };
 
 
 /**
  * Output
  * Reference: https://webpack.js.org/configuration/output/
  */
-config.output = TEST ?
-	{} :
-	{
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'js/[name].bundle.js'
-	};
+config.output = TEST ? {} : {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].bundle.js'
+};
 
 
 /**
@@ -59,100 +56,100 @@ config.output = TEST ?
  * Reference: https://webpack.js.org/configuration/module/
  */
 config.module = {
-	rules: [
+    rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            //Exclude the contents of node_modules except foundation-sites
+            exclude: /node_modules(?!\/foundation-sites\/)/
+        },
+        {
+            test: /\.css$/,
+            loader: TEST ?
+                'null-loader' : ExtractTextPlugin.extract({
+                    use: [{
+                            loader: 'css-loader',
+                            query: {
+                                minimize: true,
+                                sourceMap: true
+                            }
+                        },
+                        'postcss-loader'
+                    ],
+                    fallback: 'style-loader'
+                })
+        },
+        {
+            test: /\.(ico|png|svg|jpg|gif)$/,
+            use: [
+                'file-loader'
+            ]
+        },
+        {
+            test: /\.scss$/,
+            loader: TEST ?
+                'null-loader' : ExtractTextPlugin.extract({
+                    use: [{
+                            loader: "css-loader", // translates CSS into CommonJS
+                            query: {
+                                sourceMap: true,
+                                minimize: true
+                            }
+                        },
 
-		{
-			test: /\.js$/,
-			loader: 'babel-loader',
-			//Exclude the contents of node_modules except foundation-sites
-			exclude: /node_modules(?!\/foundation-sites\/)/
-		},
+                        'postcss-loader',
 
-		{
-			test: /\.css$/,
-			loader: TEST
-				? 'null-loader'
-				: ExtractTextPlugin.extract({
-					use: [
-						{ loader: 'css-loader', query: { minimize: true, sourceMap: true } },
-						'postcss-loader'
-					],
-					fallback: 'style-loader'
-				})
-		},
+                        {
+                            loader: "sass-loader", // compiles Sass to CSS
+                            options: {
+                                includePaths: [path.resolve(__dirname, "node_modules")]
+                            }
+                        },
 
-		{
-			test: /\.(ico|png|svg|jpg|gif)$/,
-			use: [
-				'file-loader'
-			]
-		},
+                    ],
 
-		{
-			test: /\.scss$/,
-			loader: TEST ?
-				'null-loader' :
-				ExtractTextPlugin.extract({
-					use: [{
-						loader: "css-loader", // translates CSS into CommonJS
-						query: {
-							sourceMap: true,
-							minimize: true
-						}
-					},
-
-					'postcss-loader',
-
-					{
-						loader: "sass-loader", // compiles Sass to CSS
-						options: {
-							includePaths: [path.resolve(__dirname, "node_modules")]
-						}
-					},
-
-					],
-
-					fallback: 'style-loader'
-				})
-		}
-	]
+                    fallback: 'style-loader'
+                })
+        }
+    ]
 }
 
 /**
-* Plugins
-* Reference: https://webpack.js.org/configuration/plugins/
-*/
+ * Plugins
+ * Reference: https://webpack.js.org/configuration/plugins/
+ */
 if (!TEST) {
-	config.plugins.push(
+    config.plugins.push(
 
-		new CleanWebpackPlugin(['dist']),
+        new CleanWebpackPlugin(['dist']),
 
-		new HtmlWebpackPlugin({
-			template: './public/index.html',
-			inject: 'body'
-		}),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            inject: 'body'
+        }),
 
-		new webpack.optimize.UglifyJsPlugin({ minimize: true }),
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true
+        }),
 
-		new webpack.optimize.CommonsChunkPlugin({
-			name: 'js/vendor.js',
-			children: true
-		}),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'js/vendor.js',
+            children: true
+        }),
 
-		new ExtractTextPlugin("css/styles.css"),
+        new ExtractTextPlugin("css/styles.css"),
 
-		new CopyWebpackPlugin([{
-				//Make sure the default facicon is availble when building for production
-				from: __dirname + '/public/favicon.ico'
-			},
-			{
-				//Handle all images when building for production
-				from: __dirname + '/public/images',
-				to: 'images/'
-			}
+        new CopyWebpackPlugin([{
+                //Make sure the default facicon is availble when building for production
+                from: __dirname + '/public/favicon.ico'
+            },
+            {
+                //Handle all images when building for production
+                from: __dirname + '/public/images',
+                to: 'images/'
+            }
 
-		])
-	);
+        ])
+    );
 }
 
 /**
@@ -161,51 +158,51 @@ if (!TEST) {
  * Type of sourcemap to use per build type
  */
 if (TEST) {
-	config.devtool = 'inline-source-map';
+    config.devtool = 'inline-source-map';
 } else if (PROD) {
-	config.devtool = 'source-map';
+    config.devtool = 'source-map';
 } else {
-	config.devtool = 'eval';
+    config.devtool = 'eval';
 }
 
 /**
-* Development server
-* Reference: https://webpack.js.org/configuration/dev-server/
-*/
+ * Development server
+ * Reference: https://webpack.js.org/configuration/dev-server/
+ */
 if (!PROD || !TEST) {
-	config.devServer = {
-		contentBase: path.join(__dirname, 'public'),
-		compress: true,
-		port: 1508,
-		overlay: true,
-		open: true,
-		openPage: '.',
-		//Turn off most output details when running webpack
-		stats: {
-			colors: true,
-			//Output bundle hash
-		    hash: false,
-			//Output webpack version
-		    version: false,
-		    timings: false,
-		    assets: true,
-		    chunks: false,
-		    modules: false,
-		    reasons: false,
-		    children: false,
-		    source: false,
-		    errors: true,
-		    errorDetails: false,
-		    warnings: false,
-		    publicPath: false
-		}
-	}
+    config.devServer = {
+        contentBase: path.join(__dirname, 'public'),
+        compress: true,
+        port: 1508,
+        overlay: true,
+        open: true,
+        openPage: '.',
+        //Turn off most output details when running webpack
+        stats: {
+            colors: true,
+            //Output bundle hash
+            hash: false,
+            //Output webpack version
+            version: false,
+            timings: false,
+            assets: true,
+            chunks: false,
+            modules: false,
+            reasons: false,
+            children: false,
+            source: false,
+            errors: true,
+            errorDetails: false,
+            warnings: false,
+            publicPath: false
+        }
+    }
 }
 
 /**
-* Stats
-* Reference: https://webpack.js.org/configuration/stats/
-*/
+ * Stats
+ * Reference: https://webpack.js.org/configuration/stats/
+ */
 config.stats = 'errors-only';
 
 
