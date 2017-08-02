413a3b9e99c3b4a34062ddf03278f49cc3ed9c2c
Sune Radich Vestergaard <srv@1508.dk>
2017-08-02 11:36:28 +0200

    Add phantomjs headline browser to test setup
    Default to run Chrome when using `karma start` directly. And PhantomJS when using `npm test`

--- karma.conf.js
+++ karma.conf.js
@@ -65,7 +65,7 @@ module.exports = function(config) {
 
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		browsers: ['Chrome'],
+		browsers: process.env.npm_package_scripts_test ? ['PhantomJS'] : ['Chrome'],
 
 
 		// Continuous Integration mode
